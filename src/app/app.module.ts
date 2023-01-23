2c9beb035166862423f006230cfab373cac3cfe9
Quim <joaquimpujol@gmail.com>
2023-01-23 19:58:41 +0100

    First Commit Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BusquedaComponent } from './gifs/busqueda/busqueda.component';
+
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    GifsModule,
+    SharedModule,
     BrowserModule
   ],
   providers: [],

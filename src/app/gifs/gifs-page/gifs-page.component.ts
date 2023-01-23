66f87f7f5239eb9a86a2cbbd036646ffb3ae6c6c
Quim <joaquimpujol@gmail.com>
2023-01-24 00:15:41 +0100

    busqueda html - ts

--- src/app/gifs/gifs-page/gifs-page.component.ts
+++ src/app/gifs/gifs-page/gifs-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-gifs-page',
   templateUrl: './gifs-page.component.html',
-  styleUrls: ['./gifs-page.component.sass']
 })
 export class GifsPageComponent {
 
